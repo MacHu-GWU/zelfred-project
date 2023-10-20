68e7e8c68675820d3c7f39133567ea66d2ab6f7b
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-10-19 21:33:51 -0400

    refactor query and debug module

--- zelfred/debug.py
+++ zelfred/debug.py
@@ -1,21 +1,39 @@
 # -*- coding: utf-8 -*-
 
+"""
+This module is used to debug ``zelfred`` library.
+"""
+
 from datetime import datetime
 from .paths import path_log_txt
 
 
 class Debugger:
+    """
+    This class is used to debug ``zelfred`` library.
+
+    :param path_log_txt: the path to the log file.
+    """
     def __init__(self):
         self.path_log_txt = path_log_txt
         self._enable = False
 
     def reset(self):
+        """
+        Remove the log file.
+        """
         self.path_log_txt.unlink(missing_ok=True)
 
     def enable(self):
+        """
+        Enable the debugger.
+        """
         self._enable = True
 
     def disable(self):
+        """
+        Disable the debugger.
+        """
         self._enable = False
 
     def _log(self, text: str):
@@ -25,6 +43,9 @@ class Debugger:
             f.write(line)
 
     def log(self, text: str):
+        """
+        Log (append a new line) a text to the log file.
+        """
         if self._enable:
             self._log(text)
 
